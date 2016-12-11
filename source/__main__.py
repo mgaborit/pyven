8e8cc12219a931af553580c3ae9dbe9208712b07
mgaborit <mgaborit@invoke.fr>
2016-12-11 23:14:27 +0100

    fix (help) : updated help display

--- source/__main__.py
+++ source/__main__.py
@@ -19,10 +19,10 @@ def main(args):
 	
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--version', action='version', version='1.0.0')
-	parser.add_argument('--display', '-d', action='store_true', help='Displays build report in the webbrowser right after build')
-	parser.add_argument('--verbose', '-v', action='store_true', help='Increases verbosity level')
-	parser.add_argument('step', choices=pyven.constants.STEPS)
-	parser.add_argument('path', nargs='?')
+	parser.add_argument('--display', '-d', action='store_true', help='display build report in the webbrowser right after build')
+	parser.add_argument('--verbose', '-v', action='store_true', help='increase verbosity level')
+	parser.add_argument('step', choices=pyven.constants.STEPS, help='pyven step to be achieved')
+	parser.add_argument('path', nargs='?', help='path to the delivery directory (used with "deliver" step only)')
 	args = parser.parse_args()
 	
 	project = Pyven(args.step, pyven.constants.VERSION, args.verbose)
