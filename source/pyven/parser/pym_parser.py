c9e974a1ec404d4a6dce0e72d3e23ce06b63b266
Martin GABORIT <mgaborit@invoke.fr>
2017-03-09 00:15:08 +0100

    feat (pym parser) : modularization

--- source/pyven/parser/pym_parser.py
+++ source/pyven/parser/pym_parser.py
@@ -26,6 +26,7 @@ class PymParser(object):
 	def __init__(self, pym='pym.xml'):
 		self.pym = pym
 		self.repo_config = 'repositories.xml'
+		self.tree = None
 		self.checker = Checker('Parser')
 		self.constants_parser = ConstantsParser('/pyven/constants/constant')
 		self.directory_repo_parser = DirectoryRepoParser('/pyven/platform[@name="'+pyven.constants.PLATFORM+'"]/repositories/repository')
@@ -37,8 +38,9 @@ class PymParser(object):
 		self.unit_tests_parser = UnitTestsParser('/pyven/platform[@name="'+pyven.constants.PLATFORM+'"]/tests/test[@type="unit"]')
 		self.valgrind_tests_parser = ValgrindTestsParser('/pyven/platform[@name="'+pyven.constants.PLATFORM+'"]/tests/test[@type="valgrind"]')
 		self.integration_tests_parser = IntegrationTestsParser('/pyven/platform[@name="'+pyven.constants.PLATFORM+'"]/tests/test[@type="integration"]')
-		
-	def _check_version(self, tree, file):
+	
+	@staticmethod
+	def check_version(tree, file):
 		doc_element = tree.getroot()
 		if doc_element is None or doc_element.tag == "name":
 			raise ParserException('[' + file + '] Missing "pyven" tag')
@@ -48,7 +50,8 @@ class PymParser(object):
 		if expected_pyven_version != pyven.constants.VERSION:
 			raise ParserException('[' + file + '] Invalid Pyven version --> Expected : ' + expected_pyven_version + ', actual : ' + pyven.constants.VERSION)
 		
-	def _parse_xml(self, file):
+	@staticmethod
+	def parse_xml(file):
 		tree = None
 		if not os.path.isfile(file):
 			raise ParserException('Configuration file not available : ' + file)
@@ -59,47 +62,94 @@ class PymParser(object):
 			pyven_exception.args = e.args
 			raise pyven_exception
 		return tree
+	
+	def check_errors(function):
+		def _intern(self):
+			try:
+				return function(self)
+			except ParserException as e:
+				self.checker.errors.append(e.args)
+				raise e
+		return _intern
+	
+	@check_errors
+	def parse_pym(self):
+		logger.info('Starting ' + self.pym + ' parsing')
+		self.tree = PymParser.parse_xml(self.pym)
+		PymParser.check_version(self.tree, self.pym)
+		return True
+		
+	@check_errors
+	def parse_constants(self):
+		return self.constants_parser.parse(self.tree)
+		
+	@check_errors
+	def parse_subprojects(self):
+		query = '/pyven/platform[@name="'+pyven.constants.PLATFORM+'"]/subprojects/subproject'
+		subprojects = []
+		for node in self.tree.xpath(query):
+			subprojects.append(node.text)
+		return subprojects
+		
+	@check_errors
+	def parse_repositories(self):
+		repo_config = os.path.join(os.environ.get('PVN_HOME'), self.repo_config)
+		tree = PymParser.parse_xml(repo_config)
+		PymParser.check_version(tree, repo_config)
+		self.directory_repo_parser.parse_available_repositories(tree)
+		return self.directory_repo_parser.parse(self.tree)
+		
+	@check_errors
+	def parse_artifacts(self):
+		return self.artifacts_parser.parse(self.tree)
+		
+	@check_errors
+	def parse_packages(self):
+		return self.packages_parser.parse(self.tree)
+		
+	@check_errors
+	def parse_preprocessors(self):
+		preprocessors = []
+		for cmake_tools in self.cmake_parser.parse(self.tree):
+			preprocessors.extend(cmake_tools)
+		return preprocessors
+		
+	@check_errors
+	def parse_builders(self):
+		builders = []
+		for msbuild_tools in self.msbuild_parser.parse(self.tree):
+			builders.extend(msbuild_tools)
+		for command_tools in self.command_parser.parse(self.tree):
+			builders.extend(command_tools)
+		return builders
+		
+	@check_errors
+	def parse_unit_tests(self):
+		return self.unit_tests_parser.parse(self.tree)
+		
+	@check_errors
+	def parse_valgrind_tests(self):
+		return self.valgrind_tests_parser.parse(self.tree)
+		
+	@check_errors
+	def parse_integration_tests(self):
+		return self.integration_tests_parser.parse(self.tree)
 		
 	def parse(self):
-		logger.info('Starting ' + self.pym + ' parsing')
-		try:
-			repo_config = os.path.join(os.environ.get('PVN_HOME'), self.repo_config)
-			tree = self._parse_xml(repo_config)
-			self._check_version(tree, repo_config)
-			self.directory_repo_parser.parse_available_repositories(tree)
-			
-			tree = self._parse_xml(self.pym)
-			self._check_version(tree, self.pym)
-			
-			constants = self.constants_parser.parse(tree)
-			
-			query = '/pyven/platform[@name="'+pyven.constants.PLATFORM+'"]/subprojects/subproject'
-			subprojects = []
-			for node in tree.xpath(query):
-				subprojects.append(node.text)
-			
-			repositories = self.directory_repo_parser.parse(tree)
-			artifacts = self.artifacts_parser.parse(tree)
-			packages = self.packages_parser.parse(tree)
-			
-			preprocessors = []
-			for cmake_tools in self.cmake_parser.parse(tree):
-				preprocessors.extend(cmake_tools)
-			
-			builders = []
-			for msbuild_tools in self.msbuild_parser.parse(tree):
-				builders.extend(msbuild_tools)
-			for command_tools in self.command_parser.parse(tree):
-				builders.extend(command_tools)
-			
-			unit_tests = self.unit_tests_parser.parse(tree)
-			valgrind_tests = self.valgrind_tests_parser.parse(tree)
-			integration_tests = self.integration_tests_parser.parse(tree)
 		
-		except ParserException as e:
-			self.checker.errors.append(e.args)
-			raise e
-			
+		self.parse_pym()
+		
+		constants = self.parse_constants()
+		subprojects = self.parse_subprojects()
+		repositories = self.parse_repositories()
+		artifacts = self.parse_artifacts()
+		packages = self.parse_packages()
+		preprocessors = self.parse_preprocessors()
+		builders = self.parse_builders()
+		unit_tests = self.parse_unit_tests()
+		valgrind_tests = self.parse_valgrind_tests()
+		integration_tests = self.parse_integration_tests()
+		
 		logger.info('pym.xml parsed successfully')
 		return {'constants' : constants,\
 				'subprojects' : subprojects,\
