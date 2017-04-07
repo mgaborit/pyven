b0044b28813cb04d5f1e4c03ded760b1ec94ddab
Martin GABORIT <mgaborit@invoke.fr>
2017-04-07 18:03:00 +0200

    fix (step, build) : removed unused imports

--- source/pyven/processing/tools/tool.py
+++ source/pyven/processing/tools/tool.py
@@ -1,27 +1,19 @@
-import subprocess, os, shutil, time
-
-from pyven.exceptions.exception import PyvenException
-
 from pyven.processing.processible import Processible
 from pyven.reporting.reportable import Reportable
 
-from pyven.logging.logger import Logger
-
 class Tool(Processible, Reportable):
-	TYPES = ['cmake', 'msbuild', 'makefile']
-	SCOPES = ['preprocess', 'build']
-	
-	def __init__(self, type, report, name, scope):
-		Processible.__init__(self)
-		Reportable.__init__(self, report)
-		self.type = type
-		self.name = name
-		self.scope = scope
+    
+    def __init__(self, type, report, name, scope):
+        Processible.__init__(self)
+        Reportable.__init__(self, report)
+        self.type = type
+        self.name = name
+        self.scope = scope
 
-	def _format_call(self):
-		raise NotImplementedError('Invalid call to ' + type(self).__name__ + ' abstract method "_format_call"')
-	
-	def clean(self, verbose=False):
-		raise NotImplementedError('Invalid call to ' + type(self).__name__ + ' abstract method "clean"')
+    def _format_call(self):
+        raise NotImplementedError('Invalid call to ' + type(self).__name__ + ' abstract method "_format_call"')
+    
+    def clean(self, verbose=False):
+        raise NotImplementedError('Invalid call to ' + type(self).__name__ + ' abstract method "clean"')
 
-	
+    
